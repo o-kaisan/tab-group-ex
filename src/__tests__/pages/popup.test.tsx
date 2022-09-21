07e710651644ff75c8cb84694485d72190644b79
kai.tom <tkai0216@hotmail.com>
2022-09-21 11:42:18 +0900

    ポップアップメニューのスナップショットをとるように

--- src/__tests__/pages/popup.test.tsx
+++ src/__tests__/pages/popup.test.tsx
@@ -1,23 +1,16 @@
 /**
  * @jest-environment jsdom
- *
- * TODO: メニューにテキストが表示されているか
- * TODO: onClickに渡した関数が呼ばれるか
- *
  */
 
  import React from 'react';
- import ReactDOM from "react-dom";
  import renderer from 'react-test-renderer'
  import PopupMenu from '../../pages/popup';
 
  describe('PopupMenu-test', () => {
-     test('menu', () =>{
-     // ToDo: ポップアップメニューのsnapshotをとって差分が見れるように
-     // FIXME: 「Target container is not a DOM element.」テストできない。
-    //  const tree = renderer
-    //      .create(<PopupMenu/>)
-    //      .toJSON();
-    //  expect(tree).toMatchSnapshot();
+     test('メニューの表示内容が正しいことのテスト', () => {
+     const tree = renderer
+         .create(<PopupMenu/>)
+         .toJSON();
+     expect(tree).toMatchSnapshot();
      });
  });
