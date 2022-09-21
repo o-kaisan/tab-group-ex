3e2b867efa3235ba3e8215475d828b65f6ab4fa2
kai.tom <tkai0216@hotmail.com>
2022-09-21 12:19:57 +0900

    変数に型を指定

--- src/utils/tabGroups.ts
+++ src/utils/tabGroups.ts
@@ -9,8 +9,8 @@
  * アクティブなウィンドのタブを取得する
  * ※タブが取得できなかった場合は空の配列を返す。
  */
-async function getTabs (targetTabConditions: any) {
-    const tabs = await chrome.tabs.query(targetTabConditions);
+async function getTabs (targetTabConditions: chrome.tabs.QueryInfo) {
+    const tabs: chrome.tabs.Tab[] = await chrome.tabs.query(targetTabConditions);
     return tabs
 };
 
@@ -19,7 +19,7 @@ async function getTabs (targetTabConditions: any) {
  * ※タブが取得できなかった場合は空の配列を返す。
  */
 function getTabIdList(targetTabList: any) {
-    const tabIdList = Array();
+    const tabIdList: number[] = Array();
     targetTabList.forEach((tab: any) => {
         tabIdList.push(tab.id);
     });
@@ -31,19 +31,16 @@ function getTabIdList(targetTabList: any) {
  * 引数で受け取ったタブを配列ごとにグループ化する
  */
 async function groupTabs(tabIdList: number[]) {
-    console.log("タブをグループ化")
     if (tabIdList.length > 0){
-        const groupId = await chrome.tabs.group({tabIds: tabIdList});
+        const groupId: number = await chrome.tabs.group({tabIds: tabIdList});
     }
-    console.log("タブをグループ化完了")
 }
 
 /*
  * タブを全てグループ化
  */
 export function groupAllActivateTabs() {
-    console.log("タブをすべてグループ化");
-    const targetTabConditions: any = {
+    const targetTabConditions: chrome.tabs.QueryInfo = {
         currentWindow: true,
         pinned: false,
         url: ['http://*/*', 'https://*/*'],
@@ -51,17 +48,14 @@ export function groupAllActivateTabs() {
     };
     // TODO プロミス地獄解消
     getTabs(targetTabConditions).then((tabs) => {
-        const tabIdList = getTabIdList(tabs);
+        const tabIdList: number[] = getTabIdList(tabs);
         groupTabs(tabIdList).then(() => {
-            console.log("グループ化完了")
         }).catch((error) =>{
             console.log(error)
         });
     }).catch((error) => {
         console.log(error)
     });
-    // TODO グループの名前を決めたい
-    // TODO グループの色をつけたい
 }
 
 /*
@@ -77,16 +71,14 @@ async function ungroupTabs(tabIdList: number[]) {
  * 全てのグループ化を解除
  */
 export function ungroupAllTabs() {
-    console.log("グループ化解除開始")
-    const targetTabConditions: any = {
+    const targetTabConditions: chrome.tabs.QueryInfo = {
         currentWindow: true,
         pinned: false,
         url: ['http://*/*', 'https://*/*'],
     };
     getTabs(targetTabConditions).then((tabs) => {
-        const tabIdList = getTabIdList(tabs);
+        const tabIdList: number[] = getTabIdList(tabs);
         ungroupTabs(tabIdList).then(() => {
-            console.log("グループ化解除完了")
         }).catch((error) =>{
             console.log(error)
         });
