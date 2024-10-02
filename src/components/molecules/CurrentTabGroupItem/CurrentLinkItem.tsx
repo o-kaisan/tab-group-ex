02e6c8a0c4067688798230e4f901baef3781739e
o-kaisan <tkai0216@hotmail.com>
2024-10-03 00:36:30 +0900

    url一覧にfaviconを表示するようにした

--- src/components/molecules/CurrentTabGroupItem/CurrentLinkItem.tsx
+++ src/components/molecules/CurrentTabGroupItem/CurrentLinkItem.tsx
@@ -10,6 +10,7 @@ export default function CurrentLinkItem(props: Props): JSX.Element {
 
     return (
         <ListItemButton sx={{ pl: 4 }}>
+            <img style={{margin: '0 0.5rem 0 0', width: '1.2rem'}} src={props.tab.favIconUrl} alt={props.tab.favIconUrl}/>
             <ListItemText primary={props.tab.title} />
         </ListItemButton>
     )
